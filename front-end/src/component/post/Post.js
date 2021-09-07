d3c3d16955bf8a24736fc891f674201faf927dda
rajbir21-droid <banerjeerajbir2888@gmail.com>
2021-09-07 18:33:06 +0530

    Post.js-update

--- front-end/src/component/post/Post.js
+++ front-end/src/component/post/Post.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import './post.css';
 export default function Post({ post }) {
-    const PF = "http://localhost:5000/images/";
+    const PF = "https://blogscope.herokuapp.com/images/";
     return (
       <div className="post">
         {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
